2b577ac92e362bddf76405229b5596959ecdee03
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-03-21 16:25:49 +0800

    feat: 🎸 adjust

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,47 +1,57 @@
-import ky from "ky";
+// import ky from "ky";
+// import { GET_OG_CONTENT } from "../App";
+// ky.get("https://v.qq.com/x/cover/mzc00200auwca9q/r0045mxxntl.html")
+//   .then((response) => {
+//     // console.log("response", response);
+//     return response.body as ReadableStream;
+//   })
+//   .then((rb) => {
+//     const reader = rb.getReader();
+//     return new ReadableStream({
+//       start(controller) {
+//         // The following function handles each data chunk
+//         function push() {
+//           // "done" is a Boolean and value a "Uint8Array"
+//           reader.read().then(({ done, value }) => {
+//             // If there is no more data to read
+//             if (done) {
+//               // console.log("done", done);
+//               controller.close();
+//               return;
+//             }
+//             // Get the data and send it to the browser via the controller
+//             controller.enqueue(value);
+//             // Check chunks by logging to the console
+//             // console.log(done, value);
+//             push();
+//           });
+//         }
+//         push();
+//       },
+//     });
+//   })
+//   .then((stream) =>
+//     // Respond with our stream
+//     new Response(stream, { headers: { "Content-Type": "text/html" } }).text()
+//   )
+//   .then((result) => {
+//     // Do things with result
+//     // console.log("result", result);
 
-ky.get("https://www.google.com")
-  .then((response) => response.body as ReadableStream)
-  .then((rb) => {
-    const reader = rb.getReader();
-    return new ReadableStream({
-      start(controller) {
-        // The following function handles each data chunk
-        function push() {
-          // "done" is a Boolean and value a "Uint8Array"
-          reader.read().then(({ done, value }) => {
-            // If there is no more data to read
-            if (done) {
-              console.log("done", done);
-              controller.close();
-              return;
-            }
-            // Get the data and send it to the browser via the controller
-            controller.enqueue(value);
-            // Check chunks by logging to the console
-            console.log(done, value);
-            push();
-          });
-        }
-        push();
-      },
-    });
-  })
-  .then((stream) =>
-    // Respond with our stream
-    new Response(stream, { headers: { "Content-Type": "text/html" } }).text()
-  )
-  .then((result) => {
-    // Do things with result
-    console.log("result", result);
-  });
+//     var parser = new DOMParser();
+//     var doc = parser.parseFromString(result, "text/html");
+//     // console.log("doc", doc);
 
-// .then((res) => {
-//   const reader = res.body.getReader();
-//   console.log("res", reader, res.body);
-// });
-
-// pass the link directly
-// getLinkPreview("https://www.youtube.com/watch?v=MejbOFk7H6c").then((data) =>
-//   console.debug(data)
-// );
+//     // 获取所有meta标签
+//     const metaTags = doc.getElementsByTagName("meta");
+//     console.log("metaTags", metaTags);
+//     // 循环遍历所有meta标签
+//     for (let i = 0; i < metaTags.length; i++) {
+//       // 检查meta标签是否有og属性
+//       if (GET_OG_CONTENT.includes(metaTags[i].getAttribute("property") ?? "")) {
+//         // 如果找到og属性，则获取其内容并将其存储在变量中
+//         const ogContent = metaTags[i].getAttribute("content");
+//         console.log(metaTags[i].getAttribute("property"), ogContent);
+//       }
+//     }
+//   });
