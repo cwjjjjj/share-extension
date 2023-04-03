9c9737258cf2244561908fd8e8077a6b935821e3
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-04-03 18:33:42 +0800

    feat: 🎸 test

--- src/popup/pages/Mine.tsx
+++
@@ -1,3 +0,0 @@
-export default function Mine() {
-  return <div>Mine</div>;
-}
--- src/popup/utils/index.ts
+++
@@ -1,4 +0,0 @@
-import { axiosInstance } from './interceptor'
-
-export default axiosInstance
-export const { post, patch, get } = axiosInstance
