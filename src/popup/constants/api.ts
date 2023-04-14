465cb7b6f9d61612d2167cfe66b565731af0ff6e
cwjjjjj <93369602+cwjjjjj@users.noreply.github.com>
2023-04-14 11:57:03 +0800

    feat: 🎸 add api

---
+++ src/popup/constants/api.ts
@@ -0,0 +1,15 @@
+export const SEND_VERIFY_CODE = "auth/sms/sendVerifyCode";
+
+export const LOGIN_OR_SIGNUP = "auth/sms/loginOrSignup";
+
+export const CREATE_POST = "post/create";
+
+export const REMOVE_POST = "post/remove";
+
+export const POST_LIST = "post/list";
+
+export const SUBSCRIBE = "subscribe/create";
+
+export const SUBSCRIBE_LIST = "subscribe/list";
+
+export const SUBSCRIBE_REMOVE = "subscribe/remove";
